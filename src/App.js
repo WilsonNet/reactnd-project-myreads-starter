cc8c571c250bf499c46d0af82c566654d3da3de8
Wilson Neto <wilsonpsn@hotmail.com>
2019-02-11 17:48:57 -0200

    ListBooks receiving books from  API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 import ListBooks from './ListBooks';
 import SearchBooks from './SearchBooks';
 
-class BooksApp extends React.Component {
+class BooksApp extends Component {
   state = {
     books: []
     /**
@@ -16,17 +16,21 @@ class BooksApp extends React.Component {
      */
   }
   componentDidMount() {
-    BooksAPI.getAll().then(books => console.log(books))
+    BooksAPI.getAll().then(books => this.setState({ books }))
   }
 
   render() {
-      return(
-        <div className="app">
-          {/* <Route path="/search-books" component={SearchBooks}>
+    return (
+      <div className="app">
+        {/* <Route path="/search-books" component={SearchBooks}>
         </Route> */}
-          <Route path="/" component={ListBooks} />
-        </div>
-      )
+        <Route path="/" render={() => (
+          <ListBooks
+            books={this.state.books}
+          />
+        )} />
+      </div>
+    )
   }
 }
 
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -2,8 +2,12 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
 class ListBooks extends Component {
+    state = {
+
+    }
 
     render() {
+        const { books } = this.props
         return (
             <div className="list-books">
                 <div className="list-books-title">
