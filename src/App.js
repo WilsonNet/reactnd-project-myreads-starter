eb0364e8230d7098c91a5a43e3e13a4c5f5b6850
Wilson Neto <wilsonpsn@hotmail.com>
2019-02-11 16:48:17 -0200

    Getting all books from API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
-import SearchBooks from './src/SearchBooks.js'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
-import ListBooks from './ListBooks.js';
+import ListBooks from './ListBooks';
+import SearchBooks from './SearchBooks';
 
 class BooksApp extends React.Component {
   state = {
+    books: []
     /**
      * TODO: Instead of using this state variable to keep track of which page
      * we're on, use the URL in the browser's address bar. This will ensure that
@@ -14,19 +15,18 @@ class BooksApp extends React.Component {
      * pages, as well as provide a good URL they can bookmark and share.
      */
   }
+  componentDidMount() {
+    BooksAPI.getAll().then(books => console.log(books))
+  }
 
   render() {
-    return (
-      <div className="app">
-        <Route path="/search-books" component={SearchBooks}>
-        </Route>
-
-        {/* <Route path="/" component={ListBooks} /> */}
-
-
-
-      </div>
-    )
+      return(
+        <div className="app">
+          {/* <Route path="/search-books" component={SearchBooks}>
+        </Route> */}
+          <Route path="/" component={ListBooks} />
+        </div>
+      )
   }
 }
 
