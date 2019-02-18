28c3d3c9d4e596a53b2cb3984feccacff3b89d5f
Wilson Neto <wilsonpsn@hotmail.com>
2019-02-17 21:28:56 -0300

    Debbuged search

--- src/BooksGrid.js
+++ src/BooksGrid.js
@@ -14,7 +14,7 @@ function BooksGrid(props) {
                 <li key={book.id}>
                     <div className="book">
                         <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
+                            {book.imageLinks && (<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>) }
                             <div className="book-shelf-changer">
                                 <select value={book.shelf} onChange={e => updateBook(e, book)}>
                                     <option value="move" disabled>Move to...</option>
